d330dbe4ca9b5b2bb23efceb8f62ff2d8d18e39b
Professor-Jalloh <alhajijalloh920@gmail.com>
2024-11-04 23:15:44 +0000

    Create User Input.py
    Assignment done

---
+++ User Input.py
@@ -0,0 +1,10 @@
+# Greetings & Name
+greeting = 'Hello, what is your name?'
+name = 'Lamarana Bah'
+print(greeting, name)
+#Age
+age = '15'
+print(f'How old are you?' , age)
+# Location
+location = 'Demoudoula'
+print(f'Where do you live?' , location)
